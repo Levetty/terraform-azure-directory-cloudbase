82c0beb7b88dd670fe14963b19374e780e2fe5e0
tsutsun17 <110manns@gmail.com>
2025-06-10 14:28:34 +0900

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,20 @@
-// output
 output "cloudbase_app_application_id" {
   value = module.cloudbase-app.cloudbase_app_application_id
 }
 
 output "directory_id" {
   value = var.directory_id
+}
+
+// constants
+output "cspm_role_def_name" {
+  value = var.cspm_permissions.custom.role_def_name
+}
+
+output "cwpp_role_def_name" {
+  value = var.cwpp_permissions.custom.role_def_name
+}
+
+output "auto_role_assignment_deployment_role_def_name" {
+  value = var.auto_role_assignment_deployment_permissions.custom.role_def_name
 }
